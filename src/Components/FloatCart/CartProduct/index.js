2448c09ec5b81b8e9fdcfd5cb5b8ffb58f307c12
jinpyorha <jinpyorha@gmail.com>
2019-02-06 10:35:21 -0600

    .

--- src/Components/FloatCart/CartProduct/index.js
+++ src/Components/FloatCart/CartProduct/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 import Thumb from '../../Thumb';
 import { formatPrice } from '../../../utils/utils';
--- src/Components/FloatCart/index.js
+++ src/Components/FloatCart/index.js
@@ -1,5 +1,4 @@
 import React, { Component} from 'react';
-import PropTypes from 'prop-types';
 import CartProduct from './CartProduct';
 import { formatPrice } from '../../utils/utils';
 import './style.scss';
