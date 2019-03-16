56b8b37adc2b53b6e781cc6ece0510cff563648f
jinpyorha <jinpyorha@gmail.com>
2019-03-15 23:51:03 -0500

    z

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const formatPrice = (x, currency) => {
   switch (currency) {
-    case 'USD':
+    case 'KRW':
       return x.toFixed(3).replace('.', ',');
     default:
       return x.toFixed(2);
